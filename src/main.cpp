386beadbb638fa421d233f3b18d83343e90949a9
Guilherme Biancardi <guilhermeaw.com@gmail.com>
2023-04-14 14:51:11 -0300

    Foi devolvido o código correto, o código anterior foi um teste para emulação de Tag.

--- src/main.cpp
+++ src/main.cpp
@@ -51,51 +51,294 @@ bool NFCBlockConnection(int block, int keyType, uint8_t* key);
 */
 
 void setup(void) {
+
     Serial.begin(9600);
-    while (!Serial) delay(10); // for Leonardo/Micro/Zero
 
-    Serial.println("Hello!");
+    // Para Leonardo/Micro/Zero
+    // while (!Serial) delay(10);
 
+    // Inicia o Leitor
     nfc.begin();
 
-    uint32_t versiondata = nfc.getFirmwareVersion();
-    if (!versiondata) {
-        Serial.print("Didn't find PN53x board");
-        while (1); // halt
+    // Imprime dados da placa
+    NFCHardwareInfos();
+
+}
+
+void loop(void) {
+
+    // 16 Bytes (caracteres) por bloco
+    uint8_t data[17] = { "@GuilhermeAw.com" };
+
+    // KeyA chave padrão
+    uint8_t KeyA[6] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+
+    // KeyB chave padrão
+    uint8_t KeyB[6] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+
+    // Verifica se a Tag foi reconhecida.
+    if (NFCTagConnection()) {
+        // Verifica se o UID tem 4 bytes e verifica se o setor do bloco informado foi autenticado
+        if (NFCBlockConnection(4, 0, KeyA)) {
+            // Escreve dados no bloco 4
+            NFCWriteTag4Bytes(4, data);
+            // Lê os dados do Bloco 4
+            NFCReadTag4Bytes(4);
+            // Lê todos os dados disponíveis
+            NFCTagDumpInfo4Bytes(KeyB);
+        } else {
+            // UID 7 bytes
+            NFCReadTag7Bytes(4);
+        }
     }
 
-    // Got ok data, print it out!
+    delay(2000);
+
+}
+
+void NFCHardwareInfos() {
+
+    // Armazena dados de versão se o leitor for reconhecido.
+    uint32_t versiondata = nfc.getFirmwareVersion();
+    if (!versiondata) {
+        Serial.print("Não foi possível encontrar uma placa PN53x");
+        while (1);
+    }
+
+    // Tudo OK, Imprime algumas informações da placa.
     Serial.print("Found chip PN5"); Serial.println((versiondata >> 24) & 0xFF, HEX);
     Serial.print("Firmware ver. "); Serial.print((versiondata >> 16) & 0xFF, DEC);
     Serial.print('.'); Serial.println((versiondata >> 8) & 0xFF, DEC);
 
-    // Set the max number of retry attempts to read from a card
-    // This prevents us from waiting forever for a card, which is
-    // the default behaviour of the PN532.
-    nfc.setPassiveActivationRetries(0xFF);
-
-    Serial.println("As Target... Approach the NFC PN532 Board to a PoS or terminal!");
-    delay(200);
 }
 
-void loop(void) {
-    uint8_t apdubuffer[255] = {}, apdulen = 0;
-    uint8_t ppse[] = { 0x8E, 0x6F, 0x23, 0x84, 0x0E, 0x32, 0x50, 0x41, 0x59, 0x2E, 0x53, 0x59, 0x53, 0x2E, 0x44, 0x44, 0x46, 0x30, 0x31, 0xA5, 0x11, 0xBF, 0x0C, 0x0E, 0x61, 0x0C, 0x4F, 0x07, 0xA0, 0x00, 0x00, 0x00, 0x03, 0x10, 0x10, 0x87, 0x01, 0x01, 0x90, 0x00 };
-    nfc.AsTarget();
-    (void)nfc.getDataTarget(apdubuffer, &apdulen); //Read initial APDU
-    if (apdulen > 0) {
-        for (uint8_t i = 0; i < apdulen; i++) {
-            Serial.print(" 0x"); Serial.print(apdubuffer[i], HEX);
-        }
+bool NFCTagConnection() {
+
+    // Armazena dados da conexão
+    uint8_t connection;
+
+    // Aguarda uma Tag ISO14443A (Mifare, etc.) ser encontrada pela placa.
+    // Quando encontrada armazenará o UID da Tag na variável uidLength
+    // Se o UID tiver 4 bytes = (Mifare Classic) ou 7 bytes = (Mifare Ultralight)
+    connection = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+
+    if (connection) {
+
+        // Imprime informações da Tag encontrada.
+        Serial.println("Encontrado Tag ISO14443A");
+        Serial.print("-- Tamanho da UID: "); Serial.print(uidLength, DEC); Serial.println(" bytes");
+        Serial.print("-- Valor da UID: ");
+        nfc.PrintHex(uid, uidLength);
         Serial.println("");
+
+        return true;
+
+    } else {
+
+        return false;
+
     }
-    nfc.setDataTarget(ppse, sizeof(ppse));   //Mimic a smart card response with a PPSE APDU
-    nfc.getDataTarget(apdubuffer, &apdulen); //Read respond from the PoS or Terminal
-    if (apdulen > 0) {
-        for (uint8_t i = 0; i < apdulen; i++) {
-            Serial.print(" 0x"); Serial.print(apdubuffer[i], HEX);
+
+}
+
+bool NFCBlockConnection(int block, int keyType, uint8_t* key) {
+
+    // Armazena dados da conexão
+    uint8_t connection;
+
+    if (uidLength == 4) {
+
+        Serial.println("Tag Mifare Classic (4 byte UID)");
+
+        if (keyType == 0) {
+            Serial.println("Tentando autenticar o bloco informado com a chave padrão KeyA.");
         }
-        Serial.println("");
+
+        if (keyType == 1) {
+            Serial.println("Tentando autenticar o bloco informado com a chave padrão KeyB.");
+        }
+
+        // Tenta autenticar o setor do bloco informado com a KeyA
+        connection = nfc.mifareclassic_AuthenticateBlock(uid, uidLength, block, keyType, key);
+
+        if (connection) {
+            Serial.println("Bloco autenticado.");
+            return true;
+        } else {
+            Serial.println("Erro na autenticação.");
+            return false;
+        }
+
+    } else {
+        return false;
     }
-    delay(1000);
+
+}
+
+// Ignora os blocos que contém as chaves de acesso e as informações do fabricante.
+bool IgnoreReservedBlocks(int AtualBlock) {
+    bool found = false;
+    // Blocos reservados
+    int array[] = { 0, 3, 7, 11, 15, 19, 23, 27, 31, 35, 39, 43, 47, 51, 55, 59, 63 };
+    for (int i = 0; i < 17; i++) {
+        if (array[i] == AtualBlock) {
+            found = true;
+            break;
+        }
+    }
+    return found;
+}
+
+void NFCWriteTag4Bytes(int block, uint8_t data[17]) {
+
+    // Armazena dados da conexão
+    uint8_t connection;
+
+    if (!IgnoreReservedBlocks(block)) {
+
+        connection = nfc.mifareclassic_WriteDataBlock(4, data);
+
+        if (connection) {
+            Serial.println("Dados armazenados no bloco informado.");
+        } else {
+            Serial.println("Ooops ... Não foi possível escrever no bloco.");
+        }
+
+    } else {
+        Serial.println("O bloco informado é reservado para a Key do Setor.");
+    }
+
+}
+
+void NFCReadTag4Bytes(int block) {
+
+    // Armazena dados da conexão
+    uint8_t connection;
+
+    // Armazena os dados lidos.
+    uint8_t data[16];
+
+    // Tenta ler o conteúdo do bloco 4
+    connection = nfc.mifareclassic_ReadDataBlock(block, data);
+
+    if (connection) {
+
+        Serial.println("Dados contidos no bloco informado:");
+        nfc.PrintHexChar(data, 16);
+        Serial.println("");
+        // Espero um pouco antes de ler o bloco novamente
+        delay(1000);
+
+    } else {
+        Serial.println("Ooops ... Não foi possível autenticar o bloco. Tente outra Key.");
+    }
+
+}
+
+void NFCReadTag7Bytes(int page) {
+
+    // Armazena dados da conexão
+    uint8_t connection;
+
+    if (uidLength == 7) {
+
+        Serial.println("Tag Mifare Ultralight (7 byte UID)");
+
+        // Tenta ler a página 4
+        Serial.println("Lendo página.");
+        uint8_t data[32];
+        connection = nfc.mifareultralight_ReadPage(page, data);
+
+        if (connection) {
+
+            // Dados sendo lidos
+            nfc.PrintHexChar(data, page);
+            Serial.println("");
+            // Espero um pouco antes de ler a página novamente
+            delay(1000);
+
+        } else {
+            Serial.println("Ooops ... Não foi possível ler a página.");
+        }
+
+    }
+
+}
+
+void NFCTagDumpInfo4Bytes(uint8_t* key) {
+
+    // Armazena dados da conexão
+    uint8_t connection;
+
+    // Bloco Atual
+    uint8_t currentblock;
+
+    // Armazena se o bloco está autenticado
+    bool authenticated = false;
+
+    // Armazena os dados do bloco
+    uint8_t data[16];
+
+    for (currentblock = 0; currentblock < 64; currentblock++) {
+
+        // Check if this is a new block so that we can reauthenticate
+        if (nfc.mifareclassic_IsFirstBlock(currentblock)) {
+            authenticated = false;
+        }
+
+        // Se o setor não tiver sido autenticado, faz isso primeiro
+        if (!authenticated) {
+
+            // Starting of a new sector ... try to to authenticate
+            Serial.print("----------------------- Setor ");
+            Serial.print(currentblock / 4, DEC);
+            Serial.println("------------------------");
+
+            // Chave padrão KeyA (0xFF 0xFF 0xFF 0xFF 0xFF 0xFF) para Mifare Classic
+            // Ou KeyA (0xA0 0xA1 0xA2 0xA3 0xA4 0xA5) para NDEF Tags,
+            // Mas o KeyB é o mesmo para os 2 (0xFF 0xFF 0xFF 0xFF 0xFF 0xFF)
+            connection = nfc.mifareclassic_AuthenticateBlock(uid, uidLength, currentblock, 1, key);
+
+            if (connection) {
+                authenticated = true;
+            } else {
+                Serial.println("Erro na authenticação.");
+            }
+        }
+
+        // Se não estiver autenticado, pula o bloco
+        if (!authenticated) {
+
+            Serial.print("Block ");
+            Serial.print(currentblock, DEC);
+            Serial.println(" não foi possível autenticar.");
+
+        } else {
+
+            // Autenticado, lê o bloco
+            connection = nfc.mifareclassic_ReadDataBlock(currentblock, data);
+
+            if (connection) {
+
+                Serial.print("Block ");
+                Serial.print(currentblock, DEC);
+
+                if (currentblock < 10) {
+                    Serial.print("  ");
+                } else {
+                    Serial.print(" ");
+                }
+
+                nfc.PrintHexChar(data, 16);
+
+            } else {
+                Serial.print("Block ");
+                Serial.print(currentblock, DEC);
+                Serial.println("  não foi possível ler o bloco.");
+            }
+
+        }
+
+    }
+
 }
